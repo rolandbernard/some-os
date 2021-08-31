13a5369ab0bf64e0564138a95bda39ef88ba8832
Roland Bernard <rolli.bernard@gmail.com>
2021-08-31 22:36:48 +0200

    Added panic if initializing fails

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -35,21 +35,24 @@ void kernelMain() {
     // Initialize baseline devices
     status = initBaselineDevices();
     if (isError(status)) {
-        KERNEL_LOG("[+] Failed to initialize baseline devices: %s", getErrorMessage(status));
+        KERNEL_LOG("[!] Failed to initialize baseline devices: %s", getErrorMessage(status));
+        panic();
     } else {
         KERNEL_LOG("[+] Kernel started");
     }
     // Initialize kernel systems
     status = initAllSystems();
     if (isError(status)) {
-        KERNEL_LOG("[+] Failed to initialize kernel: %s", getErrorMessage(status));
+        KERNEL_LOG("[!] Failed to initialize kernel: %s", getErrorMessage(status));
+        panic();
     } else {
         KERNEL_LOG("[+] Kernel initialized");
     }
     // Initialize devices
     status = initDevices();
     if (isError(status)) {
-        KERNEL_LOG("[+] Failed to initialize devices: %s", getErrorMessage(status));
+        KERNEL_LOG("[!] Failed to initialize devices: %s", getErrorMessage(status));
+        panic();
     } else {
         KERNEL_LOG("[+] Devices initialized");
     }
