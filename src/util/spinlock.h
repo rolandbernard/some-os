4568d747d56b50f684d788230c7d3fd91080c104
Roland Bernard <rolli.bernard@gmail.com>
2021-09-07 20:57:48 +0200

    Adjusted the spinlock implementation

--- src/util/spinlock.h
+++ src/util/spinlock.h
@@ -1,10 +1,15 @@
 #ifndef _SPINLOCK_H_
 #define _SPINLOCK_H_
 
+#include <stdbool.h>
+
 typedef int SpinLock;
 
 // Lock simple spinlock with fences
 void lockSpinLock(SpinLock* lock);
+
+// Try to lock, and return true if locking succeeded, false if it fails
+bool tryLockingSpinLock(SpinLock* lock);
 
 // Unlock simple spinlock with fences
 void unlockSpinLock(SpinLock* lock);
