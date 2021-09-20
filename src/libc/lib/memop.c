82b75197dce72f575a52b7f0ae344c9387438600
Roland Bernard <rolli.bernard@gmail.com>
2021-09-20 21:33:34 +0200

    Fixed bugs in the memset and memmove implementation

--- src/libc/lib/memop.c
+++ src/libc/lib/memop.c
@@ -3,19 +3,21 @@
 #include <stddef.h>
 
 void* memset(void* mem, int byte, size_t n) {
-    while ((uintptr_t)mem % 8 != 0) {
+    while (n > 0 && (uintptr_t)mem % 8 != 0) {
         *((uint8_t*)mem) = byte;
         mem++;
         n--;
     }
-    uint64_t big = 0;
-    for (int i = 0; i < 8; i++) {
-        big |= ((uint64_t)byte & 0xff) << (8 * i);
-    }
-    while (n > 8) {
-        *((uint64_t*)mem) = big;
-        mem += 8;
-        n -= 8;
+    if (n > 8) {
+        uint64_t big = 0;
+        for (int i = 0; i < 8; i++) {
+            big |= ((uint64_t)byte & 0xff) << (8 * i);
+        }
+        while (n > 8) {
+            *((uint64_t*)mem) = big;
+            mem += 8;
+            n -= 8;
+        }
     }
     while (n > 0) {
         *((uint8_t*)mem) = byte;
@@ -27,7 +29,7 @@ void* memset(void* mem, int byte, size_t n) {
 
 void* memmove(void* dest, const void* src, size_t n) {
     if (dest < src) {
-        while ((uintptr_t)dest % 8 != 0 && (uintptr_t)src % 8 != 0) {
+        while (n > 0 && ((uintptr_t)dest % 8 != 0 || (uintptr_t)src % 8 != 0)) {
             (*(uint8_t*)dest) = *(uint8_t*)src;
             dest++;
             src++;
@@ -48,7 +50,7 @@ void* memmove(void* dest, const void* src, size_t n) {
     } else {
         dest += n;
         src += n;
-        while ((uintptr_t)dest % 8 != 0 && (uintptr_t)src % 8 != 0) {
+        while (n > 0 && ((uintptr_t)dest % 8 != 0 || (uintptr_t)src % 8 != 0)) {
             dest--;
             src--;
             (*(uint8_t*)dest) = *(uint8_t*)src;
