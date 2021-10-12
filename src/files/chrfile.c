5f727364e1bb8181596017c481bef87c37daa8e8
Roland Bernard <rolli.bernard@gmail.com>
2021-10-12 21:09:44 +0200

    Implemented character files for serial devices

---
+++ src/files/chrfile.c
@@ -0,0 +1,86 @@
+
+#include <string.h>
+
+#include "files/chrfile.h"
+
+#include "memory/kalloc.h"
+
+static void serialReadFunction(SerialDeviceFile* file, Uid uid, Gid gid, VirtPtr buffer, size_t size, VfsFunctionCallbackSizeT callback, void* udata) {
+    Error status;
+    size_t i;
+    lockSpinLock(&file->lock);
+    for (i = 0; i < size; i++) {
+        char c;
+        status = file->serial.read(file->serial.data, &c);
+        if (status.kind != SUCCESS) {
+            break;
+        }
+        writeIntAt(buffer, 8, i, c);
+    }
+    unlockSpinLock(&file->lock);
+    if (status.kind == NO_DATA && i != 0) {
+        callback(simpleError(SUCCESS), i, udata);
+    } else {
+        // Success or any other error
+        callback(status, i, udata);
+    }
+}
+
+static void serialWriteFunction(SerialDeviceFile* file, Uid uid, Gid gid, VirtPtr buffer, size_t size, VfsFunctionCallbackSizeT callback, void* udata) {
+    lockSpinLock(&file->lock);
+    for (size_t i = 0; i < size; i++) {
+        file->serial.write(file->serial.data, readIntAt(buffer, i, 8));
+    }
+    unlockSpinLock(&file->lock);
+    callback(simpleError(SUCCESS), size, udata);
+}
+
+static void serialStatFunction(SerialDeviceFile* file, Uid uid, Gid gid, VfsFunctionCallbackStat callback, void* udata) {
+    lockSpinLock(&file->lock);
+    VfsStat ret = {
+        // TODO: use real values?
+        .id = 0,
+        .mode = TYPE_MODE(VFS_TYPE_CHAR) | VFS_MODE_OG_RW,
+        .nlinks = 0,
+        .uid = 0,
+        .gid = 0,
+        .size = 0,
+        .block_size = 0,
+        .st_atime = 0,
+        .st_mtime = 0,
+        .st_ctime = 0,
+    };
+    unlockSpinLock(&file->lock);
+    callback(simpleError(SUCCESS), ret, udata);
+}
+
+static void serialCloseFunction(SerialDeviceFile* file, Uid uid, Gid gid, VfsFunctionCallbackVoid callback, void* udata) {
+    lockSpinLock(&file->lock);
+    dealloc(file);
+    callback(simpleError(SUCCESS), udata);
+}
+
+static void serialDupFunction(SerialDeviceFile* file, Uid uid, Gid gid, VfsFunctionCallbackFile callback, void* udata) {
+    lockSpinLock(&file->lock);
+    SerialDeviceFile* copy = kalloc(sizeof(SerialDeviceFile));
+    memcpy(copy, file, sizeof(SerialDeviceFile));
+    unlockSpinLock(&file->lock);
+    unlockSpinLock(&copy->lock); // Also unlock the new file
+    callback(simpleError(SUCCESS), (VfsFile*)copy, udata);
+}
+
+static const VfsFileVtable functions = {
+    .read = (ReadFunction)serialReadFunction,
+    .write = (WriteFunction)serialWriteFunction,
+    .stat = (StatFunction)serialStatFunction,
+    .close = (CloseFunction)serialCloseFunction,
+    .dup = (DupFunction)serialDupFunction,
+};
+
+SerialDeviceFile* createSerialDeviceFile(Serial serial) {
+    SerialDeviceFile* file = zalloc(sizeof(SerialDeviceFile));
+    file->base.functions = &functions;
+    file->serial = serial;
+    return file;
+}
+
---
+++ src/files/chrfile.h
@@ -0,0 +1,18 @@
+#ifndef _CHRFILE_H_
+#define _CHRFILE_H_
+
+#include "files/vfs.h"
+#include "devices/serial/serial.h"
+#include "util/spinlock.h"
+
+// File wrapper around single byte read and write functions
+
+typedef struct {
+    VfsFile base;
+    SpinLock lock;
+    Serial serial;
+} SerialDeviceFile;
+
+SerialDeviceFile* createSerialDeviceFile(Serial serial);
+
+#endif
