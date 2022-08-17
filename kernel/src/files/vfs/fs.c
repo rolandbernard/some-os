b3b8114aeb8c12315a8bdb4efa2bda888ab6ff28
Roland Bernard <rolli.bernard@gmail.com>
2022-08-17 23:55:02 +0200

    Implemented unlink vfs functionality

--- kernel/src/files/vfs/fs.c
+++ kernel/src/files/vfs/fs.c
@@ -120,6 +120,55 @@ static Error vfsLookupNode(Process* process, VfsNode* curr, const char* path, Vf
     return err;
 }
 
+static Error vfsLookupNodeAtAbs(
+    VirtualFilesystem* fs, Process* process, const char* path, VfsNode** ret, char** real_path
+) {
+    lockSpinLock(&fs->lock);
+    VfsSuperblock* root_mount = fs->root_mounted;
+    if (root_mount != NULL) {
+        vfsSuperCopy(root_mount);
+        unlockSpinLock(&fs->lock);
+        VfsNode* root = root_mount->root_node;
+        vfsNodeCopy(root);
+        vfsSuperClose(root_mount);
+        Error err = vfsLookupNode(process, root, path, ret, real_path);
+        vfsNodeClose(root);
+        return err;
+    } else {
+        unlockSpinLock(&fs->lock);
+        return simpleError(ENOENT);
+    }
+}
+
+static Error vfsLookupNodeAt(
+    VirtualFilesystem* fs, Process* process, VfsFile* file, const char* path, VfsNode** ret, char** real_path
+) {
+    if (path[0] == '/') {
+        return vfsLookupNodeAtAbs(fs, process, path, ret, real_path);
+    } else {
+        char* absolute_path;
+        size_t cwd_length;
+        size_t path_length = strlen(path);
+        if (file == NULL) {
+            lockSpinLock(&process->resources.lock);
+            cwd_length = strlen(process->resources.cwd);
+            absolute_path = kalloc(cwd_length + path_length + 2);
+            memcpy(absolute_path, process->resources.cwd, cwd_length);
+            unlockSpinLock(&process->resources.lock);
+        } else {
+            cwd_length = strlen(file->path);
+            absolute_path = kalloc(cwd_length + path_length + 2);
+            memcpy(absolute_path, file->path, cwd_length);
+        }
+        absolute_path[cwd_length] = '/';
+        memcpy(absolute_path + cwd_length + 1, path, path_length);
+        absolute_path[cwd_length + path_length + 1] = 0;
+        Error err = vfsLookupNodeAtAbs(fs, process, absolute_path, ret, real_path);
+        dealloc(absolute_path);
+        return err;
+    }
+}
+
 static Error vfsCreateDirectoryDotAndDotDot(VfsNode* dir_node, VfsNode* parent) {
     // TODO: Should this be handled by the concrete filesystem?
     // These links are created by the system regardless of user permissions.
@@ -128,11 +177,13 @@ static Error vfsCreateDirectoryDotAndDotDot(VfsNode* dir_node, VfsNode* parent) 
     return simpleError(SUCCESS);
 }
 
-static Error vfsCreateNewNode(Process* process, VfsNode* root, const char* path, VfsMode mode, VfsNode** ret, char** real_path) {
+static Error vfsCreateNewNode(VirtualFilesystem* fs, Process* process, VfsFile* file, const char* path, VfsMode mode, VfsNode** ret, char** real_path) {
     char* parent_path = getParentPath(path);
     VfsNode* parent;
     char* real_parent_path = NULL;
-    CHECKED(vfsLookupNode(process, root, parent_path, &parent, real_path != NULL ? &real_parent_path : NULL), dealloc(parent_path))
+    CHECKED(vfsLookupNodeAt(fs, process, file, parent_path, &parent, real_path != NULL ? &real_parent_path : NULL), {
+        dealloc(parent_path);
+    });
     dealloc(parent_path);
     VfsNode* new;
     CHECKED(vfsSuperNewNode(parent->superblock, &new), {
@@ -151,6 +202,13 @@ static Error vfsCreateNewNode(Process* process, VfsNode* root, const char* path,
         vfsNodeClose(new);
         dealloc(real_parent_path);
     });
+    if (MODE_TYPE(mode) == VFS_TYPE_DIR) {
+        CHECKED(vfsCreateDirectoryDotAndDotDot(new, parent), {
+            vfsNodeClose(parent);
+            vfsNodeClose(new);
+            dealloc(real_parent_path);
+        });
+    }
     vfsNodeClose(parent);
     *ret = new;
     if (real_path != NULL) {
@@ -222,84 +280,129 @@ static Error vfsOpenNode(Process* process, VfsNode* node, char* path, VfsOpenFla
     }
 }
 
-static Error vfsOpen(VirtualFilesystem* fs, Process* process, const char* path, VfsOpenFlags flags, VfsMode mode, VfsFile** ret) {
-    assert(path[0] == '/');
-    lockSpinLock(&fs->lock);
-    VfsSuperblock* root_mount = fs->root_mounted;
-    if (root_mount != NULL) {
-        vfsSuperCopy(root_mount);
-        unlockSpinLock(&fs->lock);
-        VfsNode* root = root_mount->root_node;
-        vfsNodeCopy(root);
-        vfsSuperClose(root_mount);
-        VfsNode* node;
-        char* real_path;
-        Error err = vfsLookupNode(process, root, path, &node, &real_path);
-        if (err.kind == ENOENT && (flags & VFS_OPEN_CREATE) != 0) {
-            // Open can open special files but not create them.
-            VfsMode file_mode = (mode & ~VFS_MODE_TYPE) | TYPE_MODE(VFS_TYPE_REG);
-            Error err = vfsCreateNewNode(process, root, path, file_mode, &node, &real_path);
-            vfsNodeClose(root);
-            if (isError(err)) {
-                vfsNodeClose(node);
-                dealloc(real_path);
-                return err;
-            } else {
-                return vfsOpenNode(process, node, real_path, flags, ret);
-            }
+Error vfsOpenAt(VirtualFilesystem* fs, Process* process, VfsFile* file, const char* path, VfsOpenFlags flags, VfsMode mode, VfsFile** ret) {
+    VfsNode* node;
+    char* real_path;
+    Error err = vfsLookupNodeAt(fs, process, file, path, &node, &real_path);
+    if (err.kind == ENOENT && (flags & VFS_OPEN_CREATE) != 0) {
+        // Open can open special files but not create them.
+        VfsMode file_mode = (mode & ~VFS_MODE_TYPE) | TYPE_MODE(VFS_TYPE_REG);
+        Error err = vfsCreateNewNode(fs, process, file, path, file_mode, &node, &real_path);
+        if (isError(err)) {
+            vfsNodeClose(node);
+            dealloc(real_path);
+            return err;
         } else {
-            vfsNodeClose(root);
-            if (isError(err)) {
-                vfsNodeClose(node);
-                dealloc(real_path);
-                return err;
-            } else if ((flags & VFS_OPEN_EXCL) != 0) {
-                vfsNodeClose(node);
-                dealloc(real_path);
-                return simpleError(EEXIST);
-            } else {
-                return vfsOpenNode(process, node, real_path, flags, ret);
-            }
+            return vfsOpenNode(process, node, real_path, flags, ret);
         }
+    } else if (isError(err)) {
+        vfsNodeClose(node);
+        dealloc(real_path);
+        return err;
+    } else if ((flags & VFS_OPEN_EXCL) != 0) {
+        vfsNodeClose(node);
+        dealloc(real_path);
+        return simpleError(EEXIST);
     } else {
-        unlockSpinLock(&fs->lock);
-        return simpleError(ENOENT);
+        return vfsOpenNode(process, node, real_path, flags, ret);
     }
 }
 
-Error vfsOpenAt(VirtualFilesystem* fs, Process* process, VfsFile* file, const char* path, VfsOpenFlags flags, VfsMode mode, VfsFile** ret) {
-    if (path[0] == '/') {
-        return vfsOpen(fs, process, path, flags, mode, ret);
+Error vfsMknodAt(VirtualFilesystem* fs, Process* process, VfsFile* file, const char* path, VfsMode mode, DeviceId id) {
+    if (MODE_TYPE(mode) != VFS_TYPE_REG && MODE_TYPE(mode) != VFS_TYPE_DIR && MODE_TYPE(mode) != VFS_TYPE_FIFO) {
+        // All of these are unspecified in posix. We don't support them.
+        return simpleError(EINVAL);
     } else {
-        char* absolute_path;
-        size_t cwd_length;
-        size_t path_length = strlen(path);
-        if (file == NULL) {
-            lockSpinLock(&process->resources.lock);
-            cwd_length = strlen(process->resources.cwd);
-            absolute_path = kalloc(cwd_length + path_length + 2);
-            memcpy(absolute_path, process->resources.cwd, cwd_length);
-            unlockSpinLock(&process->resources.lock);
+        VfsNode* node;
+        Error err = vfsLookupNodeAt(fs, process, file, path, &node, NULL);
+        if (err.kind == ENOENT) {
+            Error err = vfsCreateNewNode(fs, process, file, path, mode, &node, NULL);
+            if (isError(err)) {
+                return err;
+            } else {
+                vfsNodeClose(node);
+                return simpleError(SUCCESS);
+            }
+        } else if (isError(err)) {
+            return err;
         } else {
-            cwd_length = strlen(file->path);
-            absolute_path = kalloc(cwd_length + path_length + 2);
-            memcpy(absolute_path, file->path, cwd_length);
+            vfsNodeClose(node);
+            return simpleError(EEXIST);
         }
-        absolute_path[cwd_length] = '/';
-        memcpy(absolute_path + cwd_length + 1, path, path_length);
-        absolute_path[cwd_length + path_length + 1] = 0;
-        Error err = vfsOpen(fs, process, absolute_path, flags, mode, ret);
-        dealloc(absolute_path);
-        return err;
     }
 }
-/* if (MODE_TYPE(mode) == VFS_TYPE_DIR) { */
-/*     CHECKED(vfsCreateDirectoryDotAndDotDot(new, parent), { */
-/*         vfsNodeClose(parent); */
-/*         vfsNodeClose(new); */
-/*         dealloc(real_parent_path); */
-/*     }); */
-/* } */
+
+static Error vfsRemoveDirectoryDotAndDotDot(Process* process, VfsNode* parent, VfsNode* dir) {
+    size_t max_size = sizeof(VfsDirectoryEntry) + 3;
+    size_t offset = 0;
+    size_t tmp_size = 0;
+    VfsDirectoryEntry* entry = kalloc(max_size + 3);
+    do {
+        CHECKED(vfsNodeReaddirAt(dir, process, virtPtrForKernel(entry), offset, max_size, &tmp_size), dealloc(entry));
+        if (entry->len > max_size || (strcmp(entry->name, ".") != 0 && strcmp(entry->name, "..") != 0)) {
+            dealloc(entry);
+            return simpleError(EEXIST);
+        }
+    } while (tmp_size != 0);
+    dealloc(entry);
+    lockSpinLock(&dir->lock);
+    if (dir->nlinks == 2) {
+        dir->nlinks -= 1;
+        unlockSpinLock(&dir->lock);
+        CHECKED(vfsNodeUnlink(dir, process, "."));
+        CHECKED(vfsSuperWriteNode(dir));
+        lockSpinLock(&parent->lock);
+        parent->nlinks -= 1;
+        unlockSpinLock(&parent->lock);
+        CHECKED(vfsNodeUnlink(dir, process, ".."));
+        CHECKED(vfsSuperWriteNode(parent));
+        return simpleError(SUCCESS);
+    } else {
+        unlockSpinLock(&dir->lock);
+        // If we are here, there must be more than one link to this directory.
+        // This is not well support right now. (".." entry might be wrong now.)
+        return simpleError(SUCCESS);
+    }
+}
+
+Error vfsUnlinkFrom(Process* process, VfsNode* parent, const char* filename, VfsNode* node) {
+    if (MODE_TYPE(node->mode) == VFS_TYPE_DIR) {
+        CHECKED(vfsNodeUnlink(parent, process, filename));
+        CHECKED(vfsRemoveDirectoryDotAndDotDot(process, parent, node));
+    } else {
+        CHECKED(vfsNodeUnlink(parent, process, filename));
+    }
+    lockSpinLock(&node->lock);
+    node->nlinks--;
+    unlockSpinLock(&node->lock);
+    return vfsSuperWriteNode(node);
+}
+
+Error vfsUnlinkAt(VirtualFilesystem* fs, Process* process, VfsFile* file, const char* path, VfsUnlinkFlags flags) {
+    char* parent_path = getParentPath(path);
+    VfsNode* parent;
+    CHECKED(vfsLookupNodeAt(fs, process, file, parent_path, &parent, NULL), dealloc(parent_path));
+    dealloc(parent_path);
+    const char* filename = getBaseFilename(path);
+    VfsNode* node;
+    CHECKED(vfsNodeLookup(parent, process, filename, &node), vfsNodeClose(parent));
+    Error err = vfsUnlinkFrom(process, parent, filename, node);
+    vfsNodeClose(node);
+    vfsNodeClose(parent);
+    return err;
+}
+
+Error vfsLinkAt(VirtualFilesystem* fs, Process* process, VfsFile* old_file, const char* old, VfsFile* new_file, const char* new) {
+}
+
+Error vfsMount(VirtualFilesystem* fs, const char* path, VfsSuperblock* sb) {
+}
+
+Error vfsUmount(VirtualFilesystem* fs, const char* path) {
+}
+
+Error vfsCreateSuperblock(VirtualFilesystem* fs, const char* path, const char* type, VirtPtr data, VfsSuperblock** ret) {
+}
 
 bool canAccess(VfsMode mode, Uid file_uid, Gid file_gid, struct Process_s* process, VfsAccessFlags flags) {
     if (process == NULL || process->resources.uid == 0) {
