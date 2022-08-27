d7ce301f025714864ef56886ae49ff79f8e5361a
Roland Bernard <rolli.bernard@gmail.com>
2022-08-27 18:56:28 +0200

    Improved kernel log debug location info

--- kernel/src/error/log.c
+++ kernel/src/error/log.c
@@ -5,24 +5,65 @@
 
 #include "error/log.h"
 
-#include "process/types.h"
-#include "util/text.h"
 #include "devices/devices.h"
 #include "devices/serial/serial.h"
+#include "error/debuginfo.h"
 #include "interrupt/syscall.h"
 #include "memory/virtptr.h"
+#include "process/types.h"
 #include "task/syscall.h"
+#include "util/text.h"
 
 static SpinLock kernel_log_lock;
 
-Error logKernelMessage(const char* fmt, ...) {
+static void logDebugLocation(uintptr_t addr) {
+    logKernelMessage(STYLE_DEBUG " ∟ at \e[m" STYLE_DEBUG_EXT "%p \e[m", addr);
+    SymbolDebugInfo* symb_info = searchSymbolDebugInfo(addr - 1);
+    if (symb_info != NULL) {
+        logKernelMessage(
+            STYLE_DEBUG " (\e[m" STYLE_DEBUG_EXT "%s\e[m" STYLE_DEBUG "+%p)\e[m",
+            symb_info->symbol, addr - symb_info->addr
+        );
+    }
+    LineDebugInfo* line_info = searchLineDebugInfo(addr - 1);
+    if (line_info != NULL) {
+        logKernelMessage(STYLE_DEBUG_LOC " %s:%d", line_info->file, line_info->line);
+    }
+    logKernelMessage("\e[m\n");
+}
+
+Error logKernelMessageV(const char* fmt, va_list args) {
     // Logging happens to the default serial device
     Serial serial = getDefaultSerialDevice();
-    FORMAT_STRING(string, fmt);
+    FORMAT_STRINGV(string, fmt, args);
     lockSpinLock(&kernel_log_lock);
     Error error = writeToSerial(serial, "%s", string);
     unlockSpinLock(&kernel_log_lock);
     return error;
+}
+
+Error logKernelMessage(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    Error error = logKernelMessageV(fmt, args);
+    va_end(args);
+    return error;
+}
+
+void logKernelMessageWithDebugLocationAt(uintptr_t pc, const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    logKernelMessageV(fmt, args);
+    va_end(args);
+    logDebugLocation(pc);
+}
+
+void logKernelMessageWithDebugLocation(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    logKernelMessageV(fmt, args);
+    va_end(args);
+    logDebugLocation((uintptr_t)__builtin_return_address(0) - 4);
 }
 
 static void* writeVirtPtrString(VirtPtrBufferPart part, void* udata) {
